195e0b313cfec60729c16f7fc6f5b0525936a19b
Have Samuel <samiehave9@gmail.com>
2023-03-31 01:30:04 +0300

    Add both red and yellow pieces to the board

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const board = document.querySelector('#board');
 // const cells = document.querySelectorAll('.empty');
 
 const playerRed = 'R'; // Red player
@@ -9,6 +10,31 @@ const gameOver = false; // Game over flag
 
 const rows = 6; // Rows
 const cols = 7; // Columns
+
+// Set the piece
+function setPiece() {
+  if (gameOver) {
+    return; // If the game is over, do nothing
+  }
+  // if game is not over, set the piece
+  // console.log(this.id);
+  const coords = this.id.split('-'); // split - '0-0' => ['0', '0']
+  // since they are strings, convert them to numbers
+  // with the parseInt() function
+  const r = parseInt(coords[0]);
+  const c = parseInt(coords[1]);
+
+  // add the piece to the board
+  board[r][c] = currentPlayer;
+  const cell = this;
+  if (currentPlayer === playerRed) {
+    // cell.classList.remove('cell-empty');
+    cell.classList.add('cell-red');
+  } else {
+    // cell.classList.remove('cell-empty');
+    cell.classList.add('cell-yellow');
+  }
+}
 
 // Set the game
 function setGame() {
@@ -23,13 +49,15 @@ function setGame() {
 
       // Add the cells to the DOM
       // <div id="0-0"class="cell-empty"></div>
-      const board = document.querySelector('#board');
       const cell = document.createElement('div');
       cell.classList.add('cell-empty');
       // Add an id to the cell for easy access
       // cell.id = `${r.toString()}-${c.toString()}`;
       cell.id = `${r}-${c}`;
       board.append(cell);
+
+      // When the cells are clicked
+      cell.addEventListener('click', setPiece);
     }
   }
 }
