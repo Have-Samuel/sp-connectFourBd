b6d3160ab1ca8f8beebabb053e4e5ad58746df3e
Have Samuel <samiehave9@gmail.com>
2023-03-30 23:27:14 +0300

    Add the clickable row

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const board = document.querySelector('#board');
+
+let playerRed = 'R'; // Red player
+let playerYellow = 'Y'; // Yellow player
+let currentPlayer = playerRed; // Current player
+
+let gameOver = false; // Game over flag
+// let board; // Board array
+
+let rows = 6; // Rows
+let cols = 7; // Columns
