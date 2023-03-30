4c904ff130db43673afef42a61c379141ecb7b5e
Have Samuel <samiehave9@gmail.com>
2023-03-30 23:45:59 +0300

    Add an ID to the cells

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const board = document.querySelector('#board');
+const cells = document.querySelectorAll('.empty');
 
-let playerRed = 'R'; // Red player
-let playerYellow = 'Y'; // Yellow player
-let currentPlayer = playerRed; // Current player
+const playerRed = 'R'; // Red player
+const playerYellow = 'Y'; // Yellow player
+const currentPlayer = playerRed; // Current player
 
-let gameOver = false; // Game over flag
+const gameOver = false; // Game over flag
 // let board; // Board array
 
-let rows = 6; // Rows
-let cols = 7; // Columns
+const rows = 6; // Rows
+const cols = 7; // Columns
+
+function setGame() {
+  board = [];
+  // Create the board
+  for (let r = 0; r < rows; r++) {
+    const row = [];
+    for (let c = 0; c < cols; c++) {
+      // Add the cells to the board
+      row.push(null);
+
+      // Add the cells to the DOM
+      const cell = document.createElement('div');
+      cell.classList.add('cell-empty');
+      // Add an id to the cell for easy access
+      // cell.id = `${r.toString()}-${c.toString()}`;
+      cell.id = `${r}-${c}`;
+    }
+  }
+}
+
+// When the cells are clicked
+cells.addEventListener('click', () => {
+  setGame();
+});
