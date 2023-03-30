25fe1fb77e888a8cb019ec6d69b2298ee336f5e6
Have Samuel <samiehave9@gmail.com>
2023-03-31 00:36:48 +0300

    Changing the display of the board

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const board = document.querySelector('#board');
-const cells = document.querySelectorAll('.empty');
+// const board = document.querySelector('#board');
+// const cells = document.querySelectorAll('.empty');
 
 const playerRed = 'R'; // Red player
 const playerYellow = 'Y'; // Yellow player
@@ -11,32 +11,40 @@ const gameOver = false; // Game over flag
 const rows = 6; // Rows
 const cols = 7; // Columns
 
+window.onload = () => {
+  setGame();
+};
+
+// Set the game
 function setGame() {
-  // board = [];
-  // Create the board array
-  for (let r = 0; r < rows; r++) {
-    const row = [];
-    for (let c = 0; c < cols; c++) {
-      // Add the cells to the board
-      row.push(null);
-
-      // Add the cells to the DOM
-      // <div id="0-0"class="cell-empty"></div>
-      const cell = document.createElement('div');
-      cell.classList.add('cell-empty');
-      // Add an id to the cell for easy access
-      // cell.id = `${r.toString()}-${c.toString()}`;
-      cell.id = `${r}-${c}`;
-      board.append(cell);
-    }
-    board.push(row);
-  }
+  board = [];
 }
+// function setGame() {
+//   // board = [];
+//   // Create the board array
+//   for (let r = 0; r < rows; r++) {
+//     const row = [];
+//     for (let c = 0; c < cols; c++) {
+//       // Add the cells to the board
+//       row.push(null);
 
-// When the cells are clicked
-cells.forEach((cell) => {
-  cell.addEventListener('click', () => {
-    // console.log(cell.id);
-    setGame();
-  });
-});
+//       // Add the cells to the DOM
+//       // <div id="0-0"class="cell-empty"></div>
+//       const cell = document.createElement('div');
+//       cell.classList.add('cell-empty');
+//       // Add an id to the cell for easy access
+//       // cell.id = `${r.toString()}-${c.toString()}`;
+//       cell.id = `${r}-${c}`;
+//       board.append(cell);
+//     }
+//     board.push(row);
+//   }
+// }
+
+// // When the cells are clicked
+// cells.forEach((cell) => {
+//   cell.addEventListener('click', () => {
+//     // console.log(cell.id);
+//     setGame();
+//   });
+// });
