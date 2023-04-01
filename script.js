452de266e5419978573c4dfe7ca10f339ac8d96f
Have Samuel <samiehave9@gmail.com>
2023-04-02 02:53:37 +0300

    Diagonal

--- script.js
+++ script.js
@@ -63,7 +63,9 @@ for (let c = 0; c < cols; c++) {
 for (let r = 0; r < rows; r++) {
   for (let c = 0; c < cols; c++) {
     if (board[r][c] !== ' ') {
-      if (board[r][c] === board[r + 1][c + 1] && board[r + 1])
+      if (board[r][c] === board[r + 1][c + 1] && board[r + 1]) {
+        
+      }
     }
   }
 }
